600af65ab6fd68632c2dbbc346134906f8627cb4
Francesco pippi <Francescopippi02@gmail.com>
2020-11-20 12:05:25 +0100

    Aggiungere i file di progetto.

---
+++ SocketClient_Pippi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient_Pippi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+
+            string strlIpAddress = "";
+            string strlPorta = "";
+            IPAddress Address = null;
+            int NPort;
+            try
+            {
+                Console.WriteLine("Inserisci ip del server");
+                strlIpAddress = Console.ReadLine();
+                Console.WriteLine("Inserisci numero porta");
+                strlPorta = Console.ReadLine();
+                if (!IPAddress.TryParse(strlIpAddress.Trim(),out Address))
+                {
+                    Console.WriteLine("Ip non è valido");
+                    return;
+                }
+                if(!int.TryParse(strlPorta,out NPort))
+                {
+                    Console.WriteLine("Porta non è valida");
+                    return;
+                }
+                if(NPort<=0||NPort>= 65536)
+                {
+                    Console.WriteLine("Porta non è valida");
+                    return;
+                }
+
+
+                Console.WriteLine("Endpoint del server "+Address.ToString()+ " "+ NPort);
+
+                client.Connect(Address,NPort);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("C'è stato un problema");
+            }
+        }
+    }
+}
